f9f4b4e9bde295ec15e5d22e682d06c716dd64ed
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-05-30 23:27:25 +0530

    Create alu.pl

---
+++ Day_1_Basics/alu.pl
@@ -0,0 +1,20 @@
+#!usr/bin/perl
+use strict;
+use warnings;
+
+my $name = "Perl";
+print "Language:$name\n";
+
+my $a = 23;
+my $b = 47;
+
+my $sum = $a + $b;
+print "Sum:$sum\n";
+my $sub = $a - $b;
+print "sub:$sub\n";
+my $mul = $a * $b;
+print "Mul:$mul\n";
+my $div = $a / $b;
+print "Div:$div\n";
+my $mod = $a % $b;
+print "Mod:$mod\n";
