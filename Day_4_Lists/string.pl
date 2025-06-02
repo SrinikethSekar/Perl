b63feefe8b10e8c2bebb7b619b32f1c40a9228be
Sriniketh Sekar <148874325+SrinikethSekar@users.noreply.github.com>
2025-06-02 22:56:22 +0530

    Create string.pl

---
+++ Day_4_Lists/string.pl
@@ -0,0 +1,11 @@
+#!usr/bin/perl
+
+use strict;
+use warnings;
+
+my $str = "1.2P34";
+ $str++ ;
+print "Value of STR: $str\n";
+
+#Argument "1.2P34" isn't numeric in preincrement (++) at string.pl line 7.
+#Value of STR: 2.2
